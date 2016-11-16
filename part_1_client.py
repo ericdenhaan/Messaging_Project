35c3ba0f097c46e99d45ab9d1c5de9369c0ef096
Eric <eric.denhaan@gmail.com>
2016-11-16 11:25:38 -0700

    sketching things out in part_1_client and part_1_server

---
+++ part_1_client.py
@@ -0,0 +1,26 @@
+import socket
+import sys
+
+#server address
+#client address
+#server port
+#client port
+
+#create the socket
+#bind the socket to the client address and port
+
+#get this client's ID
+
+#options:#send a message to the other client
+
+#send a get request to the server to pick up messages
+
+#create the message (based on the option given)
+#message format:
+#seq. no
+#type (send or get)
+#source (ID for this client)
+#dest (ID for the other client)
+#payload (the message)
+#send it to the server#wait for either the get result from the server
+#or for the ack from the other client
---
+++ part_1_server.py
@@ -0,0 +1,13 @@
+import socket
+import sys
+
+#host(server) address
+#host(server) port
+
+#put the server in listen mode, and wait for messages from clients
+
+#if a message comes in, determine its type by examining the type field
+#if the message is get, look through stored messages#send those that have the correct client in destination field
+
+#if the message is send, store the message#when we get the ack for this stored message, we delete the message
+#and forward the ack to the client
