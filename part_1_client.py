71252e7d5180bc3e4f7515a0532bb24845f949c5
Eric <eric.denhaan@gmail.com>
2016-11-16 23:24:02 -0700

    sending messages to server works, retrieving does not

--- part_1_client.py
+++ part_1_client.py
@@ -2,13 +2,13 @@ import socket
 import sys
 
 #server address
-server_address = "108.173.198.18"
+server_address = ""
 #client address
 client_address = ""
 #server port
 server_port = 2000
 #client port
-client_port = 2100
+client_port = input("Please enter a port (2100 or 2200?): ")
 
 #create the socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -26,11 +26,7 @@ while 1:
 #initialize the sequence number
 sequence_number = 0
 
-while 1:
-    #increment the sequence number
-    sequence_number += 1
-    sequence_number_string = str(sequence_number)
-    
+while 1:    
     #create an empty message
     message = ""
     
@@ -41,7 +37,7 @@ while 1:
     if(option == 1):
         #create the message (based on the option given)      
         #seq. no
-        message += sequence_number_string
+        message += "1"
         message += "/"      
         #type (send or get)
         message += "send"
@@ -73,13 +69,13 @@ while 1:
         
         #send the message
         client_socket.sendto(message.encode("utf-8"), (server_address, server_port))
-        break
+        print("Message sent!")
     
     elif(option == 2):
         #create the message (based on the option given)
         #message format:
         #seq. no
-        message += sequence_number_string
+        message += "1"
         message += "/"
         #type (send or get)
         message += "get"
@@ -98,12 +94,13 @@ while 1:
             client_socket.sendto(message.encode("utf-8"), (server_address, server_port))
             
             #process the reply (buffer 256 bits, decode the string, and split it into a list)
-            received_frame, sender_address = s.recvfrom(256)
+            received_frame, sender_address = client_socket.recvfrom(256)
             received_frame = received_frame.decode("utf-8")
-            received_frame = received_frame.split('/')
+            received_frame = received_frame.split("/")
             
             #if no messages left, break, else display the message
             if(received_frame[4] == ""):
+                print("The server has no messages for you.")
                 break
             else:
                 print("Message received from " + received_frame[2] + ":" + received_frame[4])
--- part_1_server.py
+++ part_1_server.py
@@ -2,12 +2,63 @@ import socket
 import sys
 
 #host(server) address
+host_address = ""
 #host(server) port
+host_port = 2000
+
+#create the server socket and bind it to the host address and port
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket.bind((host_address, host_port))
+
+#initialize the sequence number to 0
+sequence_number = 0
+
+#initialize a list of messages
+message_list = []
+
+#function to send a message to a client
+def findMessage(destination, list):
+    for message in list:
+        if(received_frame[3] == destination):
+            return received_frame
+
+#function to remove a buffered message
+def removeMessage(sequence_number, list):
+    for received_frame in list:
+        if(received_frame[0] == sequence_number):
+            list.remove(received_frame)
+    
 
 #put the server in listen mode, and wait for messages from clients
+print("Server is currently listening for messages from clients:")
 
-#if a message comes in, determine its type by examining the type field
-#if the message is get, look through stored messages#send those that have the correct client in destination field
+while 1:
+    #if a message comes in, determine its type by examining the type field
+    received_frame, sender_address = server_socket.recvfrom(256)
+    received_frame = received_frame.decode("utf-8")
+    received_frame = received_frame.split("/")
+    print("Incoming Message: ", received_frame)
 
-#if the message is send, store the message#when we get the ack for this stored message, we delete the message
-#and forward the ack to the client
+    #if the message is send, store the message
+    if(received_frame[1] == "send"):
+        message_list.append((str(sequence_number), received_frame[1], received_frame[2]))
+        message_list.append((received_frame[3], received_frame[4]))
+        #increment sequence number, if it reaches 100, roll over to 0
+        sequence_number += 1
+        if(sequence_number == 100):
+            sequence_number = 0
+        
+    #if the message is get, look through stored messages    #send those that have the correct client in destination field
+    if(received_frame[1] == "get"):
+        response = findMessage(received_frame[2], message_list)
+        if(response != None):
+            response = "/".join(response)
+            server_socket.sendto(response.encode("utf-8"), (sender_address[0], sender_address[1]))
+        else:
+            response = "1/send/server/" + received_frame[2] + "/"
+            server_socket.sendto(response.encode("utf-8"), (sender_address[0], sender_address[1]))
+    
+    #when we get the ack for a stored message, we delete the message
+        if(received_frame[1] == "ack"):
+            removeMessage(received_frame[0], message_list)
+        
